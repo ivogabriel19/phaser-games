f059bb03ad1d373468c24d32a9e1beba99cee841
ivogabreil19 <ivodegange@gmail.com>
2025-07-15 22:59:30 -0300

    se mueve y salta

--- pico_park/src/scenes/preload-scene.js
+++ pico_park/src/scenes/preload-scene.js
@@ -1,12 +1,14 @@
 import Phaser from "../lib/phaser.js";
 import { SCENE_KEYS } from "./scene-keys.js";
+import { config } from '../config.js';
+
 
 export default class PreloadScene extends Phaser.Scene {
   constructor() {
     super({ key: SCENE_KEYS.PRELOAD });
   }
 
-  init(){
+  init() {
     console.log("PreloadScene initialized");
   }
 
@@ -15,12 +17,26 @@ export default class PreloadScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32
     });
+
+    const g = this.make.graphics({ x: 0, y: 0 }, false);
+    g.fillStyle(0xb5651e);
+    g.fillRect(0, 0, config.width, 20);
+    g.generateTexture('floorTexture', config.width, 20);
   }
 
   create() {
-    this.player1 = this.add.sprite(100, 100, 'orange_guy')
-                  .setOrigin(0.5, 0.5)
-                  .setScale(2);
+
+    this.floor = this.physics.add.staticImage(config.width / 2, config.height - 10, 'floorTexture')
+      .setOrigin(0.5, 0.5)
+      .refreshBody();
+
+    this.player1 = this.physics.add.sprite(100, config.height - 200, 'orange_guy')
+      .setOrigin(0.5, 0.5)
+      .setScale(2);
+
+    this.player1.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player1, this.floor);
+
 
     this.anims.create({
       key: 'orange_guy-idle',
@@ -31,9 +47,37 @@ export default class PreloadScene extends Phaser.Scene {
 
     this.player1.anims.play('orange_guy-idle');
 
+    // Capturar teclas WASD
+    this.keys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+      jump: Phaser.Input.Keyboard.KeyCodes.SPACE
+    });
+
+    // Velocidad de movimiento
+    this.moveSpeed = 250;
+    this.jumpSpeed = -800;
   }
 
   update() {
+    if (this.keys.left.isDown) {
+      this.player1.setVelocityX(-this.moveSpeed);
+      this.player1.setFlipX(true);
+    } else if (this.keys.right.isDown) {
+      this.player1.setVelocityX(this.moveSpeed);
+      this.player1.setFlipX(false);
+    } else {
+      this.player1.setVelocityX(0);
+    }
+
+    // Salto (solo si está en el suelo)
+    const onGround = this.player1.body.touching.down;
+
+    if (Phaser.Input.Keyboard.JustDown(this.keys.jump) && onGround) {
+      this.player1.setVelocityY(this.jumpSpeed);
+    }
   }
 
 }
