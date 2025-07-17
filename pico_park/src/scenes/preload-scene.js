119540d08f693c16f094989fd586e6708671673c
ivogabreil19 <ivodegange@gmail.com>
2025-07-16 21:58:33 -0300

    player2

--- pico_park/src/scenes/preload-scene.js
+++ pico_park/src/scenes/preload-scene.js
@@ -17,6 +17,10 @@ export default class PreloadScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32
     });
+    this.load.spritesheet('blue_guy', 'assets/Sprite-0002.png', {
+      frameWidth: 32,
+      frameHeight: 32
+    });
 
     const g = this.make.graphics({ x: 0, y: 0 }, false);
     g.fillStyle(0xb5651e);
@@ -34,9 +38,15 @@ export default class PreloadScene extends Phaser.Scene {
       .setOrigin(0.5, 0.5)
       .setScale(2);
 
-    this.player1.setCollideWorldBounds(true);
-    this.physics.add.collider(this.player1, this.floor);
+    this.player2 = this.physics.add.sprite(200, config.height - 200, 'blue_guy')
+      .setOrigin(0.5, 0.5)
+      .setScale(2);
 
+    this.player1.setCollideWorldBounds(true);
+    this.player2.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player1, this.floor);
+    this.physics.add.collider(this.player2, this.floor);
+    this.physics.add.collider(this.player1, this.player2);
 
     this.anims.create({
       key: 'orange_guy-idle',
@@ -45,44 +55,72 @@ export default class PreloadScene extends Phaser.Scene {
       repeat: -1
     });
 
+    this.anims.create({
+      key: 'blue_guy-idle',
+      frames: this.anims.generateFrameNumbers('blue_guy', { start: 0, end: 5 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
     this.player1.anims.play('orange_guy-idle');
+    this.player2.anims.play('blue_guy-idle');
 
     // Capturar teclas WASD
     this.keys = this.input.keyboard.addKeys({
-      up: Phaser.Input.Keyboard.KeyCodes.W,
-      down: Phaser.Input.Keyboard.KeyCodes.S,
-      left: Phaser.Input.Keyboard.KeyCodes.A,
-      right: Phaser.Input.Keyboard.KeyCodes.D,
-      jump: Phaser.Input.Keyboard.KeyCodes.SPACE
+      jump1: Phaser.Input.Keyboard.KeyCodes.W,
+      left1: Phaser.Input.Keyboard.KeyCodes.A,
+      right1: Phaser.Input.Keyboard.KeyCodes.D,
+      jump2: Phaser.Input.Keyboard.KeyCodes.UP,
+      left2: Phaser.Input.Keyboard.KeyCodes.LEFT,
+      right2: Phaser.Input.Keyboard.KeyCodes.RIGHT
     });
 
     this.moveSpeed = 250;
     this.jumpSpeed = -800;
-    this.jumpCount = 0;
-    this.maxJumps = 2; // Número máximo de saltos permitidos
+    this.jumpCount1 = 0;
+    this.jumpCount2 = 0;
+    this.maxJumps = 3; // Número máximo de saltos permitidos
   }
 
   update() {
-    if (this.keys.left.isDown) {
+    if (this.keys.left1.isDown) {
       this.player1.setVelocityX(-this.moveSpeed);
       this.player1.setFlipX(true);
-    } else if (this.keys.right.isDown) {
+    } else if (this.keys.right1.isDown) {
       this.player1.setVelocityX(this.moveSpeed);
       this.player1.setFlipX(false);
     } else {
       this.player1.setVelocityX(0);
     }
-
-    // Salto (solo si está en el suelo)
-    const onGround = this.player1.body.touching.down;
-
-    if (onGround) {
-      this.jumpCount = 0; // Resetear al tocar el suelo
+    if (this.keys.left2.isDown) {
+      this.player2.setVelocityX(-this.moveSpeed);
+      this.player2.setFlipX(true);
+    } else if (this.keys.right2.isDown) {
+      this.player2.setVelocityX(this.moveSpeed);
+      this.player2.setFlipX(false);
+    } else {
+      this.player2.setVelocityX(0);
     }
 
-    if (Phaser.Input.Keyboard.JustDown(this.keys.jump) && this.jumpCount < this.maxJumps) {
+    // Salto (solo si está en el suelo)
+    const onGround1 = this.player1.body.touching.down;
+    const onGround2 = this.player2.body.touching.down;
+
+    // Resetear al tocar el suelo
+    if (onGround1) {
+      this.jumpCount1 = 0; 
+    }
+    if (onGround2) {
+      this.jumpCount2 = 0;
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.keys.jump1) && this.jumpCount1 < this.maxJumps) {
       this.player1.setVelocityY(this.jumpSpeed);
-      this.jumpCount++;
+      this.jumpCount1++;
+    }
+    if (Phaser.Input.Keyboard.JustDown(this.keys.jump2) && this.jumpCount2 < this.maxJumps) {
+      this.player2.setVelocityY(this.jumpSpeed);
+      this.jumpCount2++;
     }
   }
 
